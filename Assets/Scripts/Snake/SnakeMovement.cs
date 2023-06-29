26de4b3d52159d1a1bb906aaa605602e4b8f1c3b
Ehsan Foadian <ehsanfoadian@gmail.com>
2023-06-29 17:17:46 +0330

    Change textures pixels per units, create some palettes and implement first version of snake movement

---
+++ Assets/Scripts/Snake/SnakeMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class SnakeMovement : MonoBehaviour
+{
+    [SerializeField] string _moveInputId;
+    [SerializeField] PlayerInput _input;
+    [SerializeField] float _velocity;
+
+    private Vector2 _direction;
+
+    private void OnEnable()
+    {
+        _input.actions[_moveInputId].performed += SnakeMovement_performed;
+    }
+
+    private void OnDisable()
+    {
+        _input.actions[_moveInputId].performed -= SnakeMovement_performed;
+    }
+
+    private void SnakeMovement_performed(InputAction.CallbackContext ctx)
+    {
+        _direction = ctx.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(_direction*_velocity*Time.deltaTime);
+    }
+}
